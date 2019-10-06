1c5395a1fd7e92966f2488c8e3b6c82c705b9c35
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2019-10-07 03:13:48 +0500

    save file from url

--- index.js
+++ index.js
@@ -3,35 +3,27 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 const _ = require('lodash');
 const fs = require('fs');
+const https = require('https');
 
 let app = express();
 
-app.all('/updatelist', (req, res) => {
+app.all('/downloadfromurl', (req, res) => {
         console.log('...');
-        fs.readdir('folder/', (err, items) => {
-            fs.writeFile('folder/list.txt', '', (err) => {})
-
-            Promise.all(_.map(items, async (item) => {
-                return new Promise((resolve) => {
-                    fs.appendFile('folder/list.txt', item + '\r\n', (err) => {
-                        console.log('f');
-                        setTimeout(resolve, 1000);
-                    });   
-                })
-            }))
-            .then(() => {
-                console.log('!');
-                res.writeHead(301, {Location: '/'});
-                res.end();
-            })
-            
-        })
+        let file = fs.createWriteStream('folder/bd.jpg');
+        let stream = https.get("https://media.distractify.com/brand-img/aN4TJYbm8/1280x671/belle-delphine-bath-water-1563550232731.jpg", 
+            function(httpres) {
+                httpres.pipe(file);
+            });
+        stream.on('finish', function() {
+            res.writeHead(301, {Location: '/'});
+            res.end();
+        });
     })
 
 app.get('/', (req, res) => {
         console.log('.')
         // после нажатия в этом файле появится список файлов и папок, лежащих в папке
-        res.send('<a href="/updatelist">Обновить list.txt</a>');        
+        res.send('<a href="/downloadfromurl">download</a>');        
     })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
