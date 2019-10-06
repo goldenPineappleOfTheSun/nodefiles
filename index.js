d57fbeb56ea98d82a2748f8900d35a507fb01c3d
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2019-10-07 00:59:47 +0500

    small sample
    its not good designed
    next steps will be
    promises instead of settimeout
    and check file for existance

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
+const _ = require('lodash');
+const fs = require('fs');
 
-const app = express()
-    .get('/', (req, res) => {
-        res.send('hi');
+let app = express();
+
+app.all('/updatelist', (req, res) => {
+        fs.readdir('folder/', (err, items) => {
+            fs.writeFile('folder/list.txt', '', (err) => {})
+            _.each(items, (item) => {
+                console.log('!')
+                fs.appendFile('folder/list.txt', item + '\r\n', (err) => {})
+            });
+        })
+
+        setTimeout(function(){
+            res.writeHead(301, {Location: '/'});
+            res.end();
+        }, 1000)
     })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/', (req, res) => {
+        console.log('.')
+        // после нажатия в этом файле появится список файлов и папок, лежащих в папке
+        res.send('<a href="/updatelist">Обновить list.txt</a>');        
+    })
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
